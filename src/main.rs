01c92f38117f8736b2f5fe91a1a25c68d62acdf4
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-08-22 02:07:14 +0900

    Show result of git -v

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use std::process::Command;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(required = true, index = 1, help = "Base branch name (e.g. main, develop)")]
+    base_branch: String,
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let output = Command::new("git")
+        .arg("-v")
+        .output()
+        .unwrap();
+
+    let stdout_str = String::from_utf8(output.stdout).unwrap();
+    println!("{}", stdout_str);
 }
