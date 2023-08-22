ea336cc598d034bb4b73e43b269a5d60d3b0fd64
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-08-22 23:46:42 +0900

    Add lib.rs and move exec_command function

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
 use ansi_term::Colour::Red;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::Parser;
 use std::process::Command;
+
+use git_branch_delete_merged::exec_command;
 
 #[derive(Parser)]
 struct Args {
     #[arg(required = true, index = 1, help = "Base branch name (e.g. main, develop)")]
     base_branch: String,
-}
-
-fn exec_command(program: &str, args: &[&str]) -> Result<String> {
-    let output = Command::new(program).args(args).output();
-    let args_str = args.join(" ");
-
-    match output {
-        Ok(output) => {
-            if output.status.success() {
-                let stdout = String::from_utf8(output.stdout)?;
-                let trimmed_stdout = stdout.trim_end_matches('\n').to_string();
-                return Ok(trimmed_stdout);
-            } else {
-                let stderr = String::from_utf8(output.stderr)?;
-                return Err(anyhow!("\"{} {}\" received {}\n\n{}", program, args_str, output.status, stderr));
-            }
-        }
-        Err(err) => {
-            return Err(anyhow!("\"{} {}\" failed:\n{}", program, args_str, err));
-        }
-    }
 }
 
 fn main() -> Result<()> {
